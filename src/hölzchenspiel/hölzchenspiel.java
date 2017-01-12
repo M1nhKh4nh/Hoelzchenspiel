a374f4a374617a88861374547a3fbc7c7c480c2e
M1nhKh4nh <nguyenm@bzz.ch>
2017-01-12 09:36:20 +0100

    init

---
+++ src/hölzchenspiel/hölzchenspiel.java
@@ -0,0 +1,42 @@
+package hölzchenspiel;
+
+import java.util.Scanner;
+/**
+  * Hölzchenspiel
+  * @author  Minh Khanh Nguyen
+  * @date    03.11.15
+  * @version 1.0
+  */
+ 
+ 
+public class hölzchenspiel{
+ 
+  public static void main(String[] args) {
+    // ------ Variablen deklarieren ------
+	int anzahlHoelzchen = 21;
+	int spieler = 1;
+	Scanner in = new Scanner(System.in);
+	
+ 
+    // ------ Programmablauf ------
+	
+	while(anzahlHoelzchen >= 0){
+		int wegnehmen = 0;
+		System.out.println("Spieler " + spieler + " wieviele Hölzer nimmst du? (1-3) ( Momentan " + anzahlHoelzchen + ")");
+		 wegnehmen = in.nextInt();
+		
+		 anzahlHoelzchen = anzahlHoelzchen - wegnehmen;
+		if(anzahlHoelzchen <= 0){
+			System.out.println("Spieler " + spieler +" hat verloren");
+		}
+		
+		if(spieler == 1){
+			spieler = 2;
+		}
+		else{
+			spieler = 1;
+		}
+		
+	}
+   } 
+  }
